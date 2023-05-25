c0af13991b1c39090df540c796017895395a26cb
Shifta-Robel <robelbire16@gmail.com>
2023-05-25 15:33:04 +0300

    Added initial database schema

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,15 +1,47 @@
 CREATE DATABASE if not exists project;
 USE project;
 
-CREATE TABLE if not exists users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
+    email VARCHAR(255),
+    sex INT
+);  
+
+CREATE TABLE IF NOT EXISTS Merchants (
+    merchant_id INT PRIMARY KEY,
+    email VARCHAR(255)
+);  
+
+CREATE TABLE IF NOT EXISTS Events (
+    event_id INT PRIMARY KEY,
+    merchant_id INT, 
+    FOREIGN KEY (merchant_id) REFERENCES Merchants(merchant_id),
+    description VARCHAR(700), 
+    available_tickets INT
 );
 
-INSERT INTO users (name, password) VALUES ('admin', 'admin');
-INSERT INTO users (name, password) VALUES ('user1', 'user');
-INSERT INTO users (name, password) VALUES ('user2', 'user');
-INSERT INTO users (name, password) VALUES ('user3', 'user');
-INSERT INTO users (name, password) VALUES ('user4', 'user');
+CREATE TABLE IF NOT EXISTS Users (
+    user_id INT  PRIMARY KEY,  
+    user_name VARCHAR(255),
+    password VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES Customers(customer_id) OR REFERENCES Merchants(merchant_id)
+);
+
+CREATE TABLE IF NOT EXISTS Tickets (
+    customer_id INT,
+    event_id INT,
+    PRIMARY KEY(customer_id, event_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (event_id) REFERENCES Events(event_id) 
+    image_id INT,
+    FOREIGN KEY (image_id) REFERENCES Images(id)
+);
+
+CREATE TABLE IF NOT EXISTS Images (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  filename VARCHAR(255),
+  mimetype VARCHAR(255),
+  size INT,
+  data LONGBLOB
+);
 
