e5961ccb8e1314a7a8a2c353d2b871bba05f9af6
Shifta-Robel <robelbire16@gmail.com>
2023-05-29 11:32:06 +0300

    Changes to init.sql
    Added tags to Events. Removed Images table, add image URL inline, and fixed syntax errors. Updated tables Users, Customers, Merchants.
    Added dummy data

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -1,32 +1,46 @@
 CREATE DATABASE if not exists project;
 USE project;
 
+CREATE TABLE IF NOT EXISTS Users (
+    user_id INT PRIMARY KEY,  
+    user_name VARCHAR(255),
+    password VARCHAR(255),
+    user_type ENUM('customer', 'merchant') NOT NULL,
+    UNIQUE KEY (user_name)
+);
+
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     email VARCHAR(255),
-    sex INT
-);  
+    sex INT,
+    FOREIGN KEY (customer_id) REFERENCES Users(user_id)
+);
 
 CREATE TABLE IF NOT EXISTS Merchants (
     merchant_id INT PRIMARY KEY,
-    email VARCHAR(255)
-);  
+    email VARCHAR(255),
+    FOREIGN KEY (merchant_id) REFERENCES Users(user_id)
+);
 
 CREATE TABLE IF NOT EXISTS Events (
     event_id INT PRIMARY KEY,
     merchant_id INT, 
     FOREIGN KEY (merchant_id) REFERENCES Merchants(merchant_id),
     description VARCHAR(700), 
-    available_tickets INT
-    image_id INT,
-    FOREIGN KEY (image_id) REFERENCES Images(id)
+    available_tickets INT,
+    image VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Users (
-    user_id INT  PRIMARY KEY,  
-    user_name VARCHAR(255),
-    password VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES Customers(customer_id) OR REFERENCES Merchants(merchant_id)
+CREATE TABLE Tags (
+    tag_id INT PRIMARY KEY,
+    tag_name VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Event_Tags (
+    event_id INT,
+    tag_id INT,
+    FOREIGN KEY (event_id) REFERENCES Events(event_id),
+    FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
 );
 
 CREATE TABLE IF NOT EXISTS Tickets (
@@ -37,11 +51,51 @@ CREATE TABLE IF NOT EXISTS Tickets (
     FOREIGN KEY (event_id) REFERENCES Events(event_id) 
 );
 
-CREATE TABLE IF NOT EXISTS Images (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  filename VARCHAR(255),
-  mimetype VARCHAR(255),
-  size INT,
-  data LONGBLOB
-);
+-- Dummy data 
+INSERT INTO Users (user_id, user_name, password, user_type) VALUES
+(1, 'Abel', 'password1', 'customer'),
+(2, 'Biruk', 'password2', 'customer'),
+(3, 'Chaltu', 'password3', 'customer'),
+(4, 'Dawit', 'password4', 'customer'),
+(5, 'Ephrem', 'password5', 'merchant'),
+(6, 'Frehiwot', 'password6', 'merchant');
+
+INSERT INTO Customers (customer_id, email, sex) VALUES
+(1, 'abel@example.com', 1),
+(2, 'biruk@example.com', 2),
+(3, 'chaltu@example.com', 0),
+(4, 'dawit@example.com', 1);
+
+INSERT INTO Merchants (merchant_id, email) VALUES
+(5, 'ephrem@example.com'),
+(6, 'frehiwot@example.com');
+
+INSERT INTO Tags (tag_id, tag_name) VALUES
+(1, 'Birthday'),
+(2, 'Wedding'),
+(3, 'Concert');
+
+INSERT INTO Events (event_id, merchant_id, description, available_tickets, image) VALUES
+(1, 5, 'John\'s Birthday Party', 50, 'example.com/birthday.jpg'),
+(2, 6, 'Sarah and Mark\'s Wedding', 100, 'example.com/wedding.jpg'),
+(3, 5, 'Summer Concert Series', 200, 'example.com/concert1.jpg'),
+(4, 5, 'Hip Hop Festival', 150, 'example.com/concert2.jpg'),
+(5, 6, 'New Year\'s Eve Bash', 75, 'example.com/newyear.jpg');
+
+INSERT INTO Event_Tags (event_id, tag_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 3),
+(5, 1),
+(5, 3);
+
+INSERT INTO Tickets (customer_id, event_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(1, 5),
+(2, 5),
+(3, 5);
 
