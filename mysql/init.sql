b619c521c3096a9841b5de833e361a8547204a8c
Shifta-Robel <robelbire16@gmail.com>
2023-05-29 10:08:56 +0300

    Removed Images table, add image URL inline, and fixed syntax errors

--- mysql/init.sql
+++ mysql/init.sql
@@ -17,9 +17,8 @@ CREATE TABLE IF NOT EXISTS Events (
     merchant_id INT, 
     FOREIGN KEY (merchant_id) REFERENCES Merchants(merchant_id),
     description VARCHAR(700), 
-    available_tickets INT
-    image_id INT,
-    FOREIGN KEY (image_id) REFERENCES Images(id)
+    available_tickets INT,
+    image VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Tags (
@@ -32,13 +31,14 @@ CREATE TABLE IF NOT EXISTS Event_Tags (
   tag_id INT,
   FOREIGN KEY (event_id) REFERENCES Events(event_id),
   FOREIGN KEY (tag_id) REFERENCES Tags(tag_id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS Users (
     user_id INT  PRIMARY KEY,  
     user_name VARCHAR(255),
     password VARCHAR(255),
-    FOREIGN KEY (user_id) REFERENCES Customers(customer_id) OR REFERENCES Merchants(merchant_id)
+    FOREIGN KEY (user_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (user_id) REFERENCES Merchants(merchant_id)
 );
 
 CREATE TABLE IF NOT EXISTS Tickets (
@@ -48,12 +48,3 @@ CREATE TABLE IF NOT EXISTS Tickets (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     FOREIGN KEY (event_id) REFERENCES Events(event_id) 
 );
-
-CREATE TABLE IF NOT EXISTS Images (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  filename VARCHAR(255),
-  mimetype VARCHAR(255),
-  size INT,
-  data LONGBLOB
-);
-
