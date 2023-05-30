935a4cada06c816625f541f5dfa488db83fb60f8
Kidus Mekuria <kidusmesfin27@gmail.com>
2023-05-30 20:37:55 +0300

    account dashboard done

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -28,16 +28,3 @@ buttons.forEach(button =>{button.addEventListener("click", () => {
     const totalSlides=button.closest("[data-carousel-slider-cont]").querySelector("[data-total-slides]");
     totalSlides.innerHTML=slides.children.length;
 });
-const primaryNav = document.querySelector(".user-sec");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-navToggle.addEventListener("click", () => {
-    const visible = primaryNav.getAttribute("data-visible") === "true";
-    if(visible){
-        primaryNav.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false);
-    }
-    else{
-        primaryNav.setAttribute("data-visible", true);
-        navToggle.setAttribute("aria-expanded", true);
-    }
-})
