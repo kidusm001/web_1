dd253d99741af7c2702a6746e021086a3b9ea0bd
nathan dawit <nathandawit555@gmail.com>
2023-05-31 12:20:16 +0300

    feat: add routes to fetch data
    add rest routes for fetching all users and/or events
    also fixed the docker compose file so no need to rebuild everytime

---
+++ backend/php/events/get_all.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+
+$user = 'root';
+$pass = 'root';
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    try {
+        $dbh = new PDO('mysql:host=db;port=3306;dbname=project', $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $users = $dbh->query('SELECT * FROM Events');
+        echo json_encode($users->fetchAll());
+        $dbh = null;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+} else {
+    http_response_code(400);
+    echo json_encode(array('message' => 'Bad Request'));
+}
