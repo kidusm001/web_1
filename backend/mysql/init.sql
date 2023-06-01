6bb7d60865feb82d3df7145e1ad42afcb5c3b1d8
Robel B. Tamirat <robelbire16@gmail.com>
2023-06-01 09:52:08 +0300

    Update dummy data

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -59,35 +59,35 @@ CREATE TABLE IF NOT EXISTS Tickets (
 );
 
 -- Dummy data 
-INSERT INTO Users (user_name, password, user_type) VALUES
-('Abel', 'password1', 'customer'),
-('Biruk', 'password2', 'customer'),
-('Chaltu', 'password3', 'customer'),
-('Dawit', 'password4', 'customer'),
-('Ephrem', 'password5', 'merchant'),
-('Frehiwot', 'password6', 'merchant');
+INSERT INTO Users (user_name, password, user_type, firstName, lastName, email) VALUES
+('Abel', 'password1', 'customer', 'Abel', 'Zewde','abel@example.com'),
+('Biruk', 'password2', 'customer', 'Biruk', 'Tamirat','biruk@example.com'),
+('Chaltu', 'password3', 'customer', 'Chaltu', 'Aramde','chaltu@example.com'),
+('Dawit', 'password4', 'customer', 'Dawit', 'Meshesha', 'dawit@example.com'),
+('Ephrem', 'password5', 'merchant', 'Ephrem', 'Diriba', 'ephrem@example.com'),
+('Frehiwot', 'password6', 'merchant', 'Frehiwot', 'Alem', 'frehiwot@example.com');
 
 INSERT INTO Customers (customer_id, email, sex) VALUES
-('Abel', 'abel@example.com', 1),
-('Biruk', 'biruk@example.com', 2),
-('Chaltu', 'chaltu@example.com', 0),
-('Dawit', 'dawit@example.com', 1);
+('Abel', 1),
+('Biruk', 2),
+('Chaltu', 0),
+('Dawit', 1);
 
 INSERT INTO Merchants (merchant_id, email) VALUES
-('Ephrem', 'ephrem@example.com'),
-('Frehiwot', 'frehiwot@example.com');
+('Ephrem'),
+('Frehiwot');
 
-INSERT INTO Tags (tag_id, tag_name) VALUES
-(1, 'Birthday'),
-(2, 'Wedding'),
-(3, 'Concert');
+INSERT INTO Tags (tag_name) VALUES
+('Birthday'),
+('Wedding'),
+('Concert');
 
-INSERT INTO Events (event_id, merchant_id, description, available_tickets, image) VALUES
-(1, 'Ephrem', 'John\'s Birthday Party', 50, 'example.com/birthday.jpg'),
-(2, 'Frehiwot', 'Sarah and Mark\'s Wedding', 100, 'example.com/wedding.jpg'),
-(3, 'Ephrem', 'Summer Concert Series', 200, 'example.com/concert1.jpg'),
-(4, 'Ephrem', 'Hip Hop Festival', 150, 'example.com/concert2.jpg'),
-(5, 'Frehiwot', 'New Year\'s Eve Bash', 75, 'example.com/newyear.jpg');
+INSERT INTO Events (merchant_id, description, available_tickets, price, image) VALUES
+('Ephrem', 'John\'s Birthday Party', 50, 34, 'example.com/birthday.jpg'),
+('Frehiwot', 'Sarah and Mark\'s Wedding', 100, 45, 'example.com/wedding.jpg'),
+('Ephrem', 'Summer Concert Series', 200, 23, 'example.com/concert1.jpg'),
+('Ephrem', 'Hip Hop Festival', 150, 67, 'example.com/concert2.jpg'),
+('Frehiwot', 'New Year\'s Eve Bash', 75, 12, 'example.com/newyear.jpg');
 
 INSERT INTO Event_Tags (event_id, tag_id) VALUES
 (1, 1),
