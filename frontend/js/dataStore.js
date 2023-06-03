61e64442aac97cd95186b93c45d463557ddb9b1c
Shifta-Robel <robelbire16@gmail.com>
2023-06-03 13:01:57 +0300

    fixed dataStore loading issue

--- frontend/js/dataStore.js
+++ frontend/js/dataStore.js
@@ -1,23 +1,60 @@
+// let dataStore = (function() {
+//   var events = null;
+//
+//   function init() {
+//     if (events === null) {
+//       fetch('http://0.0.0.0:8000/events/get_all.php')
+//         .then(response => response.json())
+//         .then(data => {
+//           events = data.map(event => {
+//              const { event_id, merchant_id, title, description, available_tickets, price, date_and_time, image } = event;
+//               return new Event(event_id, merchant_id, title, description, available_tickets, price, date_and_time, image);
+//           });
+//           console.log(events);
+//         })
+//         .catch(error => {
+//          console.error('Error fetching data:', error);
+//         });
+//     }
+//   }
+//
+//   function reload() {
+//     events = null;
+//     init();
+//   }
+//
+//   return {
+//     init: init,
+//     reload: reload,
+//     events: function() {
+//       return events;
+//     }
+//   };
+// })();
+//
+// dataStore.init();
+
 let dataStore = (function() {
   var events = null;
 
   function init() {
     if (events === null) {
-      let events = [];
-      let allEventsJsonRepsonse
-      fetch('../backend/php/events/get_all.php')
+      return fetch('http://0.0.0.0:8000/events/get_all.php')
         .then(response => response.json())
         .then(data => {
-          allEventsJsonRepsonse = data
-          console.log(data);
+          events = data.map(event => {
+             const { event_id, merchant_id, title, description, available_tickets, price, date_and_time, image } = event;
+              return new Event(event_id, merchant_id, title, description, available_tickets, price, date_and_time, image);
+          });
+          console.log(events);
+          return events;
         })
         .catch(error => {
-         console.error('Error fetching data:', error);
+          console.error('Error fetching data:', error);
+          return null;
         });
-      events = allEventsJsonResponse.map(event => {
-          const { event_id, merchant_id, title, description, available_tickets, price, date_and_time, image } = event;
-          return new Event(event_id, merchant_id, title, description, available_tickets, price, date_and_time, image);
-      });
+    } else {
+      return Promise.resolve(events);
     }
   }
 
@@ -30,7 +67,7 @@ let dataStore = (function() {
     init: init,
     reload: reload,
     events: function() {
-      return events;
+      return init();
     }
   };
 })();
--- frontend/js/home_page.js
+++ frontend/js/home_page.js
@@ -1,14 +1,17 @@
-const topEventsContainer = document.querySelector('#top-events-container')
+const topEventsContainer = document.querySelector('#top-events-section')
 const maxTopEvents = 8
-dataStore.init()
-let events = dataStore.events()
 
-topEvents = events.sort((a, b) => {
-    const aScore = a.availableTickets * a.price;
-    const bScore = b.availableTickets * b.price;
-    return bScore - aScore;
-}).slice(0,maxTopEvents)
+async function displayTopEvents() {
+  let events = await dataStore.events();
+  topEvents = events.sort((a, b) => {
+      const aScore = a.availableTickets * a.price;
+      const bScore = b.availableTickets * b.price;
+      return bScore - aScore;
+  }).slice(0,maxTopEvents);
 
-topEvents.forEach(event => {
-  topEventsContainer.appendChild( createCardComponent(event) )
-})
+  topEvents.forEach(event => {
+   topEventsContainer.appendChild( createCardComponent(event) )
+  })
+}
+
+displayTopEvents();
