65a86a8e41df122865e4426610857511cf3467a8
Shifta-Robel <robelbire16@gmail.com>
2023-06-29 09:16:17 +0300

    added memorable passwords

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -59,12 +59,13 @@ CREATE TABLE IF NOT EXISTS Tickets (
 );
 
 -- Dummy data 
+-- 45f36d1a5bac9e263237218fd7a3ce7b2827ffb0c312c9f8ae7fc4806cbd223b / 12345Abc#
 INSERT INTO Users (user_name, password, user_type, first_name, last_name, email) VALUES
 ('Abebe#2314', '45f36d1a5bac9e263237218fd7a3ce7b2827ffb0c312c9f8ae7fc4806cbd223b', 'customer', 'Abebe', 'Tadesse','abebe@example.com'),
 ('Birhanu#7865', 'a40f4e3f9b5e0a5e2e73c4bb9d7e9c4f13a012a5e5d4d76c5ea1b5a0e2a4f1c5', 'customer', 'Birhanu', 'Mengistu','birhanu@example.com'),
 ('Chaltu#8921', 'd6e2e6a88e8ec46f3b3f1d0e2b9b6f6b5c09b0be6c9b8a4a7a5c0e6be7d4d5b9', 'customer', 'Chaltu', 'Abebe','chaltu@example.com'),
 ('Dereje#4532', '4c1c121a3cc6e8a5f1d3e4a9e4e9c7e2b5fa9aaedf3b6b6d3ab7f8e8c6d6a8b6', 'customer', 'Dereje', 'Mamo', 'dereje@example.com'),
-('Eleni#9821', 'f5a3f7b31a3f5e7e9c7f1c3b8e7d5b6d1e2d8f0c6c8b2b1c3f4d9e6a8e3b7d8', 'merchant', 'Eleni', 'Tadesse', 'eleni@example.com'),
+('Eleni#9821', '45f36d1a5bac9e263237218fd7a3ce7b2827ffb0c312c9f8ae7fc4806cbd223b', 'merchant', 'Eleni', 'Tadesse', 'eleni@example.com'),
 ('Fikirte#0078', 'a7e9e0d9e8b2c3e1d3b5a1c8f0b1a2a6d1d7b9c8d8b1c7c8a2f1d4a9d0c7c4', 'merchant', 'Fikirte', 'Negash', 'fikirte@example.com'),
 ('Girma#2345', 'd9d9e3c3d5d3c2c8f6b2b4d5a0e6c3d7f7c3c7a4d2f3e7f9d6e3c6b9d2e6', 'merchant', 'Girma', 'Gebre', 'girma@example.com'),
 ('Hana#6789', 'e5c4d0d1c7e2b7c8a9e8b4d8b7d5d5d0f7e7d8c0f4c1f6a9c3d8c4d5', 'merchant', 'Hana', 'Girma', 'hana@example.com'),
